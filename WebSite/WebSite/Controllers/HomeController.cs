835ded2d2bdc2572b96d2197be2538db58072b51
ali damirli <alidamirli26@gmail.com>
2023-12-29 02:06:52 +0300

    Controller adding of Pages

--- WebSite/WebSite/Controllers/HomeController.cs
+++ WebSite/WebSite/Controllers/HomeController.cs
@@ -17,16 +17,29 @@ namespace WebSite.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Documents()
+        {
+            return View();
+        }
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Product()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
+
+        
     }
 }
